111d53bc19ee97d32e1048cd8bf94c901ff03bb2
Swapnil Kothawade <superdev@SuperMac.local>
2020-08-15 12:08:18 -0400

    BMD Eureka naming server code push

---
+++ src/main/java/com/mybank/bmsnamingserver/BmsNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.mybank.bmsnamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BmsNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsNamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybank/bmsnamingserver/BmsNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybank.bmsnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
